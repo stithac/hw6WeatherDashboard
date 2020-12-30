25e5fb5b12ace20b36b59019daf80e8baedecca2
Ashley Stith <ashleyc.stith@gmail.com>
2020-12-29 19:55:12 -0500

    Final cleanup of files and addition of comments

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ var cities = [];
 var searchBtn = $("<button>");
     searchBtn.attr("class", "fas fa-search searchBtn btn");
 
+buildDashboard(); //function invoked on page load
+
 //buildDashboard() function is called when the page loads.
 //Builds initial page with placeholder divs
 function buildDashboard(){
-
-
     console.log("*** Start of buildDashboard() function ***");
 
     $(".searchDiv").append(cityInputEl);
@@ -54,19 +54,20 @@ function buildDashboard(){
                 $(li).text(cities[i]);
                 cityList.prepend(li);
         }
-
-        getCity(cities[cities.length - 1]);
     }
 
 }//End of buildDashboard()
 
+//getCity() function gets the city from the input and creates the API query URL.  It then calls the searchCity() function with the API query URL passed in as a parameter
 function getCity(input){
-    console.log(input);
+    console.log(input); //testing
 
     $(cityListLi).text(input);
 
+    //Check to see if the input is in the cities array.  If not, the variable item will be undefined.  If it is, item will equal the city
     var item = cities.find(item => item === input);
 
+        //If the city is not in the array, push the input to the cities array and prepend to the cityList in the left nav
         if(item === undefined){
 
             cities.push(input);
@@ -80,10 +81,12 @@ function getCity(input){
 
     console.log(queryURL);
     searchCity(queryURL);
-}
+} //End of getCity()
 
+//searchCity() function takes in the query URL and makes the AJAX calls to get the weather information for that city
 function searchCity(url){
 
+    //initial call is used to get the lat and lon of the city.  That is then passed to a different API call to get more details about the city
     $.ajax({
         url: url,
         method: "GET",
@@ -96,7 +99,7 @@ function searchCity(url){
 
         var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
 
-        displayInfo(response, url);
+        displayInfo(response, url);//call displayInfo function and pass in the response object and the new API URL
     });
 
     $(cityInputEl).val("");
@@ -105,6 +108,7 @@ function searchCity(url){
 
 }
 
+//displayInfo() function makes a second API call to get more information about the city. The city information is then displayed on the page
 function displayInfo(response, url){
 
     $(".daily").empty(); //clear any information in daily divs
@@ -116,6 +120,7 @@ function displayInfo(response, url){
     }).then(function(res) {
         console.log(res);
 
+        //Assigns all appropriate values to the elements on the page
         var icon = "https://openweathermap.org/img/wn/" + res.current.weather[0].icon + ".png";
             $(weatherIcon).attr("src", icon);
             console.log(icon);
@@ -151,6 +156,7 @@ function displayInfo(response, url){
         forecastLbl.text("5-Day Forecast:");
         $(forecastHeader).append(forecastLbl);
 
+        //Update 5-day forecast elements
         for(i = 1; i < 6; i++){
             var date = new Date(res.daily[i].dt * 1000);
             console.log(date);
@@ -177,8 +183,9 @@ function displayInfo(response, url){
             $("#forecast" + i).append(humidity);
         }
     });
-}
+}//End of displayInfo()
 
+//Click event for the left-hand navigation. When an item is clicked, the getCity() function is called with the city as a parameter
 $(document).on("click",".cityLi",function(event){
 
     city = event.target.innerHTML;
@@ -200,4 +207,3 @@ $(searchBtn).on("click",function(event){
 });
 
 
-buildDashboard();
